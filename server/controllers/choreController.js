fd6b31409e2635485c738dd03e4f0f3861e009e0
ay7991 <amy.yang7991@gmail.com>
2024-01-10 14:01:23 -0500

    adjusted properties pulled from post request for create chore and moved old code from group controller to the bottom as a comment

--- server/controllers/choreController.js
+++ server/controllers/choreController.js
@@ -1,3 +1,6 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable camelcase */
+/* eslint-disable quotes */
 const db = require('../models/choreifyModels');
 const queries = require('../models/queries');
 
@@ -17,14 +20,16 @@ choreController.getChores = (req, res, next) => {
 };
 
 choreController.createChore = (req, res, next) => {
-  // paramaterize the query
-  // console.log(req.body);
-  // const arr = [title, description, group_id, chore_status, due_date, assigner_id,
-  // created_date] = req.body;
-  // console.log(arr);
+  const { title, description, due_date, assignees, group } = req.body;
 
-  // hardcoded a chore because could not receive from client side 
-  const arr = ['feed the cat', 'feed the cat her favorite cat food', 1, 'assigned', 'end of the week', 1, (new Date()).toLocaleString('en-US')];
+  const values = [title, description, due_date, assignees, group];
+  const queryStringCreateChore =
+  `INSERT INTO chores `
+  + `(title, description, due_date, assignees, group) `
+  + `VALUES ($1, $2, $3, $4, $5)`;
+
+  // check if assignees passed in are already in assignees table 
+  // populate chore table with info passed in req.body
   
   // pass the params into the function that will insert them in the query
   // then insert them into the database using db.query
@@ -40,3 +45,14 @@ choreController.createChore = (req, res, next) => {
 };
 
 module.exports = choreController;
+
+// old code
+
+  // paramaterize the query
+  // console.log(req.body);
+  // const arr = [title, description, group_id, chore_status, due_date, assigner_id,
+  // created_date] = req.body;
+  // console.log(arr);
+
+// hardcoded a chore because could not receive from client side 
+// const arr = ['feed the cat', 'feed the cat her favorite cat food', 1, 'assigned', 'end of the week', 1, (new Date()).toLocaleString('en-US')];
